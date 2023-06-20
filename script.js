cb1f89972698a2c373b002d8327fc982b75f7303
JarielGFox <crudelegio@gmail.com>
2023-06-20 16:26:32 +0200

    test send

--- script.js
+++ script.js
@@ -1,69 +1,41 @@
-let voices = [];
-speechSynthesis.addEventListener('voiceschanged', function () {
-  voices = speechSynthesis.getVoices();
-  console.log(voices);
-})
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-// Raccolgo dalla pagina gli elementi che mi servono
-const textArea = document.querySelector('textarea');
-const playButton = document.querySelector('button');
-const pitchBar = document.querySelector('input');
-const duckFigure = document.querySelector('figure');
+const micBtn = document.getElementById('microphone');
+const screen = document.getElementById('screen');
+const panelsData = document.getElementById('panels-data');
 
-// Se qualcuno clicca il bottone, fa' quello che ti dico.
-playButton.addEventListener('click', function () {
-  const textLength = textArea.value.trim().length;
-  if (textLength > 0) {
-    // allora fa' parlare la paperella!
-    talk();
-  }
-});
+const commands = ['mangia', 'balla', 'dormi'];
 
-// Preparo una funzione per far parlare la paperella
-function talk() {
-  // 1 - Recuperiamo tono di voce e testo
-  const text = textArea.value;
-  const pitch = pitchBar.value;
+// Inizializzazione
+const recognition = new SpeechRecognition();
 
-  // 2 - Preparo una frase per il Sintetizzatore vocale
-  const utterance = new SpeechSynthesisUtterance(text);
-
-  // 3 - specifichiamo altri dettagli della frase
-  utterance.volume = 1;
-  utterance.rate = 1;
-  utterance.pitch = pitch;
-
-  const femaleVoice = voices.find(function (voice) {
-    if (voice.name.includes('Elsa')) {
-      return true;
-    }
-  });
-
-  utterance.voice = femaleVoice;
-
-  // facciamo parlare la paperella
-  speechSynthesis.speak(utterance);
-
-  // Quando la paperella inizia a parlare..
-  utterance.addEventListener('start', function () {
-    // Blocco tutti i controlli
-    textArea.disabled = true;
-    pitchBar.disabled = true;
-    playButton.disabled = true;
-
-    // animiamo la paperella
-    duckFigure.classList.add('talking');
-  });
-
-  // Quando la paperella finisce di parlare..
-  utterance.addEventListener('end', function () {
-    // Blocco tutti i controlli
-    textArea.disabled = false;
-    pitchBar.disabled = false;
-    playButton.disabled = false;
-
-    // Riportiamo la paperella statica
-    duckFigure.classList.remove('talking');
-  })
-
+function onStartListening() {
+    recognition.start();    
+    panelsData.classList.add('listening');
 }
+
+function onResult(e) {    
+    const testo = e.results[0][0].transcript;
+
+    const action = commands.find(function(command) {
+        return testo.toLowerCase().includes(command);
+    });
+
+    const actionClassname = 'codigotchi-screen_' + action;
+
+    screen.classList.add(actionClassname);
+    panelsData.classList.remove('listening');
+    
+    // Mostro l'animazione della gif per 2 secondi
+    setTimeout(function() {
+        screen.classList.remove(actionClassname);
+    }, 2000);
+}
+
+function onError(e) {
+    console.error(e.error);
+}
+
+micBtn.addEventListener('click', onStartListening);
+recognition.addEventListener('result', onResult);
+recognition.addEventListener('error', onError);
